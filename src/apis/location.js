e5c3a9b77df980f8274a735519e8caf199d75674
yong <tkddbs1644@gmail.com>
2023-11-13 14:46:53 +0900

    config: change service key

--- src/apis/location.js
+++ src/apis/location.js
@@ -2,13 +2,13 @@ import { sidos } from '../_lib/data/sidos'
 
 // 나의 서비스키로 바꿀것
 const SERVICE_KEY =
-  'CbAljrrOaPLzhasvCSIYwolRL%2Bk94sHnhvlcxFd%2FTtjOW1EbiwYLg1eHmQFLBjYZfJA2BNhsuRwEjr6i6RWPqQ%3D%3D'
+  '14DjtZu4DJ5vkaRktTKgCQhFeLW2wDK0UNf3ZZ7Y7z%2BUZWd457sU%2F09AEHtcJNvgtiwMRzI8sGC0Mu0MKG82%2Bg%3D%3D'
 
 const getGugunApiUrl = (areaCode) => {
   return `https://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey=${SERVICE_KEY}&numOfRows=20&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&areaCode=${areaCode}`
 }
 
-export const fetchGetSido = async () => {
+export const fetchGetSidoListData = async () => {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve(sidos)
@@ -16,7 +16,7 @@ export const fetchGetSido = async () => {
   })
 }
 
-export const fetchGetGugun = async (sidoCode) => {
+export const fetchGetGugunListData = async (sidoCode) => {
   try {
     const res = await window.fetch(getGugunApiUrl(sidoCode))
     const data = await res.json()
